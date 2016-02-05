4b93553005db0e221311a8cbd2dbb0aa0650a871
student <student@students-mac.student.georgianc.on.ca>
2016-02-05 11:37:56 -0500

    Rebuilt layout and whole calculator

--- calculatorRework/ViewController.swift
+++ calculatorRework/ViewController.swift
@@ -19,7 +19,81 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    var isTypingNum:Bool = false
+    var firstNumber:Float! = 0
+    var secNumber:Float! = 0
+    var operation:String! = " "
+    
+    @IBOutlet weak var calcDisplay: UILabel!
+    
+    @IBAction func numTapped(sender: AnyObject) {
+        let number = sender.currentTitle!
+        
+        if isTypingNum {
+            calcDisplay.text = calcDisplay.text! + number!
+        }
+        else {
+            calcDisplay.text = number
+            isTypingNum = true
+        }
+        //print(number)
+    }
+    
+    @IBAction func equalsTap(sender: AnyObject) {
+        
+        isTypingNum = false
+        var result:Float = 0
+        
+        
+        secNumber = Float(calcDisplay.text!)
+        
+        
+        
+        if operation == "+" {
+            result = firstNumber! + secNumber!
+        }
+        else if operation == "-" {
+            result = firstNumber! - secNumber!
+        }
+        else if operation == "/" {
+            result = firstNumber! / secNumber!
+        }
+        else if operation == "*" {
+            result = firstNumber! * secNumber!
+        }
+        else if operation == "%" {
+            result = firstNumber! % secNumber!
+        }
+        //result = firstNumber! + secNumber!
+        /*print("new line")
+        print(firstNumber)
+        print(operation)
+        
+        print(secNumber)
+        
+        print(result)*/
+        
+        if operation != " " {
+            //\(firstNumber) \(operation) \(secNumber) equals
+            calcDisplay.text = "\(result)"
+        }
+        
+    }
+    @IBAction func clear(sender: AnyObject) {
+        secNumber = 0;
+        firstNumber = 0;
+        operation = " ";
+        calcDisplay.text = "0"
+    }
+    
+    @IBAction func calcTap(sender: AnyObject) {
+        isTypingNum = false
+        firstNumber = Float(calcDisplay.text!)
+        //print(firstNumber)
+        
+        operation = sender.currentTitle
+        //print(operation)
+    }
 
 }
 
